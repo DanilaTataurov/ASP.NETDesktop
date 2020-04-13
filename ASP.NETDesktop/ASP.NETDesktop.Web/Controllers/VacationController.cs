72ededf245bfd1d1f396a6f773db6a9cb17bc14b
DanilaTataurov <dan1140@yandex.ru>
2020-04-13 13:34:07 +0700

    Update vacation list to vacation chart

---
+++ ASP.NETDesktop/ASP.NETDesktop.Web/Controllers/VacationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ASP.NETDesktop.Domain.Interfaces.Services;
+
+namespace ASP.NETDesktop.Web.Controllers {
+    public class VacationController : Controller {
+        private readonly IVacationService _vacationService;
+
+        public VacationController(IVacationService vacationService) {
+            _vacationService = vacationService;
+        }
+
+        public ActionResult Chart() {
+            return View();
+        }
+
+        public JsonResult List() {
+            var vacations = _vacationService.List().Select(x => new object[] { 
+                x.Developer.FirstName + " " + x.Developer.LastName, 
+                null, 
+                x.StartDate.ToString("dd MMM") + " - " + x.EndDate.ToString("dd MMM") + ", Status: " + x.Status, 
+                x.StartDate.ToString("yyyy-MM-dd"), 
+                x.EndDate.ToString("yyyy-MM-dd")
+            });
+
+            return Json(vacations, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- ASP.NETDesktop/ASP.NETDesktop/ASP.NETDesktop/ViewModels/Vacation/VacationChartViewModel.cs
+++ ASP.NETDesktop/ASP.NETDesktop/ASP.NETDesktop/ViewModels/Vacation/VacationChartViewModel.cs
@@ -1,20 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using ASP.NETDesktop.Common.ApiModels;
-using ASP.NETDesktop.Models;
-using ASP.NETDesktop.Services.Interfaces;
 using ASP.NETDesktop.ViewModels.Base;
-using AutoMapper;
 using Prism.Commands;
 using Prism.Navigation;
 using Prism.Services;
 
 namespace ASP.NETDesktop.ViewModels.Vacation {
     public class VacationChartViewModel : BaseViewModel, INavigationAware {
-        private readonly IMapper _mapper;
-        private readonly IVacationService _vacationService;
         private readonly INavigationService _navigationService;
         private readonly IPageDialogService _pageDialogService;
 
@@ -22,21 +14,10 @@ namespace ASP.NETDesktop.ViewModels.Vacation {
         public bool IsCreate { get; set; }
         public DelegateCommand BackCommand { get; set; }
 
-        private List<VacationModel> _vacations;
-        public List<VacationModel> Vacations { get => _vacations; set => SetProperty(ref _vacations, value); }
-
-        public VacationChartViewModel(IMapper mapper, IVacationService vacationService, INavigationService navigationService, IPageDialogService pageDialogService) {
-            _mapper = mapper;
-            _vacationService = vacationService;
+        public VacationChartViewModel(INavigationService navigationService, IPageDialogService pageDialogService) {
             _navigationService = navigationService;
             _pageDialogService = pageDialogService;
             BackCommand = new DelegateCommand(BackAsync);
-            Vacations = new List<VacationModel>();
-        }
-
-        private async Task<List<VacationApiModel>> ListAsync() {
-            var result = await _vacationService.ListAsync();
-            return result.Data;
         }
 
         private async void BackAsync() {
@@ -54,9 +35,6 @@ namespace ASP.NETDesktop.ViewModels.Vacation {
         public void OnNavigatedTo(INavigationParameters parameters) {
             Id = Guid.Parse(parameters.FirstOrDefault(x => x.Key == "Id").Value.ToString());
             IsCreate = (bool)parameters.FirstOrDefault(x => x.Key == "IsCreate").Value;
-
-            var list = Task.Run(() => ListAsync());
-            Vacations = new List<VacationModel>(_mapper.Map<List<VacationModel>>(list.Result.ToList()));
         }
 
         public void OnNavigatedFrom(INavigationParameters parameters) { }
